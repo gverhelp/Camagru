f9c9793e80c5b5cd763c42d099ffac06a00a35b9
pmaldagu <pierremaldague@gmail.com>
2023-08-16 16:11:17 +0200

    no error

--- views/home.py
+++ views/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
 
-home_blueprint = Blueprint('auth', __name__)
+home_blueprint = Blueprint('home', __name__)
 
 @home_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
