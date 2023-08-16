e9a0b36a777acf1be099ddf694a92652c03f3667
pmaldagu <pierremaldague@gmail.com>
2023-08-16 15:04:24 +0200

    flask

---
+++ views/auth.py
@@ -0,0 +1,13 @@
+from app import db
+from flask import Blueprint, flash, redirect, render_template, url_for
+from flask_login import current_user, login_user, logout_user
+
+from models.user import User
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth_blueprint = Blueprint('auth', __name__)
+
+@auth_blueprint.route("/login", methods=['GET', 'POST'])
+def login():
+    return 'lol'
