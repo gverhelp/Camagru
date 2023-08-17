5ad2cb1189decddf5f4efeadf1be23e9509b03be
Garreth <garreth.verhelpen@gmail.com>
2023-08-17 18:05:35 +0200

    navbar almost done ;) ;)

--- views/home.py
+++ views/home.py
@@ -4,6 +4,5 @@ from flask_login import login_required
 home_blueprint = Blueprint('home', __name__)
 
 @home_blueprint.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
-    return render_template('home/index.html')
+    return render_template('index.html')
