724e6e142539d1236b0fcc9daa19e640e6b73dde
Garreth <garreth.verhelpen@gmail.com>
2023-08-24 16:13:59 +0200

    cleaned searchbar + added sign containers

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,9 @@
 var like = 0;
+var modal = 0;
 
 function changeFilter(hover) {
     if (hover == 1) {
-        document.getElementById('google').style.filter = "invert(18%) sepia(95%) saturate(3630%) hue-rotate(305deg) brightness(74%) contrast(100%)";
+        document.getElementById('google').style.filter = "invert(30%) sepia(84%) saturate(958%) hue-rotate(302deg) brightness(97%) contrast(105%)";
     } else {
         document.getElementById('google').style.filter = "invert(0%) sepia(3%) saturate(27%) hue-rotate(55deg) brightness(93%) contrast(100%)";
     }
@@ -11,9 +12,25 @@ function changeFilter(hover) {
 function changeLikeButton() {
     if (like == 0) {
         document.getElementById('heart').setAttribute("src", "static/img/heart-fill.png");
-        like = 1;
     } else {
         document.getElementById('heart').setAttribute("src", "static/img/heart.png"); 
-        like = 0;
     }
+    like = !like;
+}
+
+function login(type) {
+    if (modal == 0) {
+        document.getElementById('modal').style.display = 'flex';
+        if (type == 'signin') {
+            document.getElementById('signin').hidden = false;
+        }
+        else {
+            document.getElementById('signup').hidden = false;
+        }
+    } else {
+        document.getElementById('modal').style.display = 'none';
+        document.getElementById('signin').hidden = true;
+        document.getElementById('signup').hidden = true;
+    }
+    modal = !modal;
 }
