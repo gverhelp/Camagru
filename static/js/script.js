7a27b336343e06144e7768a127402d3092feec55
Garreth <garreth.verhelpen@gmail.com>
2023-09-05 17:07:08 +0200

    profile gallery grid display seems good now

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,6 @@ var like = 0;
 var modalSign = 0;
 var activeCtn = 0;
 
-postDisplay();
-changeLeftIcon('homeIcon');
-
 function changeFilter(hover) {
     if (hover == 1) {
         document.getElementById('google').style.filter = "invert(30%) sepia(84%) saturate(958%) hue-rotate(302deg) brightness(97%) contrast(105%)";
@@ -51,16 +48,16 @@ function modalSignDisplay(type) {
 function changeLeftIcon(icon) {
     document.getElementById('homeIcon').setAttribute("src", "static/img/home-empty.png");
     document.getElementById('createIcon').setAttribute("src", "static/img/post-empty.png");
-    document.getElementById('userIcon').setAttribute("src", "static/img/user-empty.png");
-    document.getElementById('notificationIcon').setAttribute("src", "static/img/notification-empty.png");
+    document.getElementById('profileIcon').setAttribute("src", "static/img/user-empty.png");
+    document.getElementById('notificationsIcon').setAttribute("src", "static/img/notification-empty.png");
     document.getElementById('settingsIcon').setAttribute("src", "static/img/setting-empty.png");
 
     if (icon == 'createIcon') {
         document.getElementById('createIcon').setAttribute("src", "static/img/post-fill.png");
-    } else if (icon == 'userIcon') {
-        document.getElementById('userIcon').setAttribute("src", "static/img/user-fill.png"); 
-    } else if (icon == 'notificationIcon') {
-        document.getElementById('notificationIcon').setAttribute("src", "static/img/notification-fill.png"); 
+    } else if (icon == 'profileIcon') {
+        document.getElementById('profileIcon').setAttribute("src", "static/img/user-fill.png"); 
+    } else if (icon == 'notificationsIcon') {
+        document.getElementById('notificationsIcon').setAttribute("src", "static/img/notification-fill.png"); 
     } else if (icon == 'settingsIcon') {
         document.getElementById('settingsIcon').setAttribute("src", "static/img/setting-fill.png"); 
     } else if (icon == 'homeIcon') {
@@ -68,9 +65,40 @@ function changeLeftIcon(icon) {
     }
 }
 
-function postDisplay() {
-    const elem = document.getElementById('anim-center');
+function setAsFlex(elem) {
+    setTimeout(function() {
+        elem.style.display = "flex";
+    });
+}
+
+function homeDisplay() {
     var html = "";
+
+    // html += '<div class="post">'
+    // html += '   <div class="post-header">'
+    // html += '       <div class="post-avatar">'
+    // html += '           <img src="static/img/pepe.jpeg" alt="Avatar" style="width: 100%; height: auto;">' 
+    // html += '       </div>'
+    // html += '       <p> Pepe <br/> Frog </p>'
+    // html += '   </div>'
+    // html += '   <div class="post-body">'
+    // html += '       <img class="post-pictures" src="static/img/pepe.jpeg">'
+    // html += '   </div>'
+    // html += '   <div class="post-footer">'
+    // html += '       <button class="post-footer-btn" onclick="changeLikeButton()">'
+    // html += '           <div class="post-footer-btn-ctn">'
+    // html += '               <img src="static/img/heart.png" id="heart" alt="like" style="width: auto; height: 80%">'
+    // html += '           1300'
+    // html += '           </div>'
+    // html += '       </button>'
+    // html += '       <button class="post-footer-btn">'
+    // html += '           <div class="post-footer-btn-ctn">'
+    // html += '               <img src="static/img/comment2.png" alt="comments" style="width: auto; height: 80%">'
+    // html += '           1340'
+    // html += '           </div>'
+    // html += '       </button>'
+    // html += '   </div>'
+    // html += '</div>'
 
     for (let i = 0; i < 5; i++) {
         html += '<div class="post">'
@@ -81,7 +109,7 @@ function postDisplay() {
         html += '       <p> Pepe <br/> Frog </p>'
         html += '   </div>'
         html += '   <div class="post-body">'
-        html += '       <img class="post-pictures" src="static/img/lol2.JPG">'
+        html += '       <img class="post-pictures" src="static/img/lol.JPG">'
         html += '   </div>'
         html += '   <div class="post-footer">'
         html += '       <button class="post-footer-btn" onclick="changeLikeButton()">'
@@ -99,64 +127,132 @@ function postDisplay() {
         html += '   </div>'
         html += '</div>'
     }
-    elem.innerHTML = html;
+
+    // html += '<div class="post">'
+    // html += '   <div class="post-header">'
+    // html += '       <div class="post-avatar">'
+    // html += '           <img src="static/img/pepe.jpeg" alt="Avatar" style="width: 100%; height: auto;">' 
+    // html += '       </div>'
+    // html += '       <p> Pepe <br/> Frog </p>'
+    // html += '   </div>'
+    // html += '   <div class="post-body">'
+    // html += '       <img class="post-pictures" src="static/img/pepe.jpeg">'
+    // html += '   </div>'
+    // html += '   <div class="post-footer">'
+    // html += '       <button class="post-footer-btn" onclick="changeLikeButton()">'
+    // html += '           <div class="post-footer-btn-ctn">'
+    // html += '               <img src="static/img/heart.png" id="heart" alt="like" style="width: auto; height: 80%">'
+    // html += '           1300'
+    // html += '           </div>'
+    // html += '       </button>'
+    // html += '       <button class="post-footer-btn">'
+    // html += '           <div class="post-footer-btn-ctn">'
+    // html += '               <img src="static/img/comment2.png" alt="comments" style="width: auto; height: 80%">'
+    // html += '           1340'
+    // html += '           </div>'
+    // html += '       </button>'
+    // html += '   </div>'
+    // html += '</div>'
+
+    return html;
 }
 
 function createDisplay() {
-    const elem = document.getElementById('anim-center');
-
     var html = "<p> Create </p>";
 
-    elem.innerHTML = html;
+    return html;
 }
 
 function profileDisplay() {
-    const elem = document.getElementById('anim-center');
-
     var html = "<p> Profile </p>";
 
-    elem.innerHTML = html;
+    return html;
 }
 
 function notifDisplay() {
-    const elem = document.getElementById('anim-center');
-
     var html = "<p> Notifications </p>";
 
-    elem.innerHTML = html;
+    return html;
 }
 
 function settingsDisplay() {
-    const elem = document.getElementById('anim-center');
-
     var html = "<p> Settings </p>";
 
-    elem.innerHTML = html;
+    return html;
 }
 
-function changePage(page) {
+// function changePage(page) {
+//     const elem = document.getElementById('anim-center');
+    
+//     elem.style.display = 'none';
+//     setAsFlex(elem);
 
-    const elem = document.querySelector('.anim-center')
-    elem.classList.add('pre-animation');
-    setTimeout(function(){
-        elem.classList.remove('pre-animation')
-    }, 200);
+//     switch (page) {
+//         case 'home':
+//             elem.innerHTML = homeDisplay();
+//             changeLeftIcon(page + 'Icon');
+//             break;
 
-    switch (page) {
-        case 'home':
-            postDisplay();
-            break;
-        case 'create':
-            createDisplay();
-            break;
-        case 'profile':
-            profileDisplay();
-            break;
-        case 'notification':
-            notifDisplay();
-            break;
-        case 'settings':
-            settingsDisplay();
-            break;
-    }
-}
+//         case 'create':
+//             elem.innerHTML = createDisplay();
+//             changeLeftIcon(page + 'Icon');
+//             break;
+
+//         case 'profile':
+//             elem.innerHTML = profileDisplay();
+//             changeLeftIcon(page + 'Icon');
+//             break;
+
+//         case 'notifications':
+//             elem.innerHTML = notifDisplay();
+//             changeLeftIcon(page + 'Icon');
+//             break;
+
+//         case 'settings':
+//             elem.innerHTML = settingsDisplay();
+//             changeLeftIcon(page + 'Icon');
+//             break;
+
+//         default:
+//             elem.innerHTML = homeDisplay();
+//             changeLeftIcon('homeIcon');
+//             break;
+//     }
+// }
+
+// changePage();
+
+// function getContent(fragmentId, callback){
+//     var pages = {
+//       home: postDisplay(),
+//       create: createDisplay(),
+//       profile: profileDisplay(),
+//       notifications: notifDisplay(),
+//       settings: settingsDisplay()
+//     };
+  
+//     callback(pages[fragmentId]);
+// }
+
+// function loadContent() {
+//     var contentDiv = document.getElementById("anim-center"),
+//         fragmentId = location.hash.substring(1);
+
+//     contentDiv.style.display = "none";
+//     changeLeftIcon(fragmentId + 'Icon');
+//     setAsFlex(contentDiv);
+  
+//     getContent(fragmentId, function (content) {
+//       contentDiv.innerHTML = content;
+//     });
+
+// }
+  
+// if(!location.hash) {
+//     location.hash = "#home";
+//     changeLeftIcon('homeIcon');
+// }
+  
+// loadContent();
+  
+// window.addEventListener("hashchange", loadContent);
