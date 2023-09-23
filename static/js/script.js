b81fbf342be6a9c6ea315ac84dfe8a60a20715cd
Garreth <garreth.verhelpen@gmail.com>
2023-09-23 19:21:03 +0200

    improved js and bottom navbar almost done

--- static/js/script.js
+++ static/js/script.js
@@ -1,88 +1,122 @@
 var themeBtn = document.querySelector("#switchThemeBtn");
-var homeBtn = document.querySelector("#homeBtn");
-var createBtn = document.querySelector("#createBtn");
-var profileBtn = document.querySelector("#profileBtn");
-var settingsBtn = document.querySelector("#settingsBtn");
+var homeBtns = document.querySelectorAll("#homeBtn");
+var createBtns = document.querySelectorAll("#createBtn");
+var profileBtns = document.querySelectorAll("#profileBtn");
+var settingsBtns = document.querySelectorAll("#settingsBtn");
 
-var icons = [document.querySelector('#logoIcon'),
-            document.querySelector('#themeIcon'),
-            document.querySelector('#homeIcon'),
-            document.querySelector('#createIcon'),
-            document.querySelector('#profileIcon'),
-            document.querySelector('#settingsIcon')]
-
-var iconsDict = {
-    'logoIcon' : 'static/img/instgram-white.png',
-    'themeIcon' : 'static/img/moon-outlined.svg',
-    'homeIcon' : 'static/img/home-outlined.svg',
-    'createIcon' : 'static/img/create-outlined.svg',
-    'profileIcon' : 'static/img/profile-outlined.svg',
-    'settingsIcon' : 'static/img/settings-outlined.svg',
-}
+var logoIcon = document.querySelector('#logoIcon');
+var themeIcon = document.querySelector('#themeIcon');
+var homeIcons = document.querySelectorAll('#homeIcon');
+var createIcons = document.querySelectorAll('#createIcon');
+var profileIcons = document.querySelectorAll('#profileIcon');
+var settingsIcons = document.querySelectorAll('#settingsIcon');
 
 const contentDiv = document.querySelector('.main-center-ctn');
 
 function changeIcon(iconType) {
 
-    let a = 0;
-    for (let key in iconsDict) {
-        if (a >= 2) {
-            icons[a].src = iconsDict[key];
-        }
-        a++;
-    }
+    homeIcons.forEach(homeIcon => {
+        homeIcon.src = 'static/img/home-outlined.svg';
+    });
 
-    document.querySelector('#' + iconType).src = 'static/img/' + iconType.substring(0, iconType.length - 4) + '-filled.svg';
+    createIcons.forEach(createIcon => {
+        createIcon.src = 'static/img/create-outlined.svg';
+    });
+
+    profileIcons.forEach(profileIcon => {
+        profileIcon.src = 'static/img/profile-outlined.svg';
+    });
+
+    settingsIcons.forEach(settingsIcon => {
+        settingsIcon.src = 'static/img/settings-outlined.svg';
+    });
+
+    document.querySelectorAll('#' + iconType).forEach(icon => {
+        icon.src = 'static/img/' + iconType.substring(0, iconType.length - 4) + '-filled.svg';
+    });
 }
 
 themeBtn.addEventListener('click', function() {
 
     if (document.body.classList.toggle('dark-mode')) {
+        homeIcons.forEach(homeIcon => {
+            homeIcon.style.filter = "invert(100%) sepia(95%) saturate(20%) hue-rotate(275deg) brightness(104%) contrast(105%)";
+        });
+    
+        createIcons.forEach(createIcon => {
+            createIcon.style.filter = "invert(100%) sepia(95%) saturate(20%) hue-rotate(275deg) brightness(104%) contrast(105%)";
+        });
+    
+        profileIcons.forEach(profileIcon => {
+            profileIcon.style.filter = "invert(100%) sepia(95%) saturate(20%) hue-rotate(275deg) brightness(104%) contrast(105%)";
+        });
+    
+        settingsIcons.forEach(settingsIcon => {
+            settingsIcon.style.filter = "invert(100%) sepia(95%) saturate(20%) hue-rotate(275deg) brightness(104%) contrast(105%)";
+        });
 
-        for (let a = 1; a < icons.length; a++) {
-            icons[a].style.filter = "invert(100%) sepia(95%) saturate(20%) hue-rotate(275deg) brightness(104%) contrast(105%)";
-        }
-        icons[0].src = "static/img/instgram-white.png";
-        icons[1].src = "static/img/sun-outlined.svg";
-
+        logoIcon.src = "static/img/instgram-white.png";
+        themeIcon.src = "static/img/sun-outlined.svg";
+        themeIcon.style.filter = "invert(100%) sepia(95%) saturate(20%) hue-rotate(275deg) brightness(104%) contrast(105%)";
     } else {
+        homeIcons.forEach(homeIcon => {
+            homeIcon.removeAttribute('style');
+        });
+    
+        createIcons.forEach(createIcon => {
+            createIcon.removeAttribute('style');
+        });
+    
+        profileIcons.forEach(profileIcon => {
+            profileIcon.removeAttribute('style');
+        });
+    
+        settingsIcons.forEach(settingsIcon => {
+            settingsIcon.removeAttribute('style');
+        });
 
-        for (let a = 1; a < icons.length; a++) {
-            icons[a].removeAttribute('style');
-        }
-        icons[0].src = "static/img/instgram-black.png";
-        icons[1].src = "static/img/moon-outlined.svg";
+        logoIcon.src = "static/img/instgram-black.png";
+        themeIcon.src = "static/img/moon-outlined.svg";
+        themeIcon.removeAttribute('style');
     }
-})
+});
 
-// homeBtn.addEventListener('click', function() {
-//     changeIcon('homeIcon');
+homeBtns.forEach(homeBtn => {
+    homeBtn.addEventListener('click', function() {
+        changeIcon('homeIcon');
 
-//     html = '<p> Home </p>';
+        html = '<p> Home </p>';
 
-//     contentDiv.innerHTML = html;
-// })
+        contentDiv.innerHTML = html;
+    });
+});
 
-// createBtn.addEventListener('click', function() {
-//     changeIcon('createIcon');
+createBtns.forEach(createBtn => {
+    createBtn.addEventListener('click', function() {
+        changeIcon('createIcon');
 
-//     html = '<p> Create </p>';
+        html = '<p> Create </p>';
 
-//     contentDiv.innerHTML = html;
-// })
+        contentDiv.innerHTML = html;
+    });
+});
 
-// profileBtn.addEventListener('click', function() {
-//     changeIcon('profileIcon');
+profileBtns.forEach(profileBtn => {
+    profileBtn.addEventListener('click', function() {
+        changeIcon('profileIcon');
 
-//     html = '<p> Profile </p>';
+        html = '<p> Profile </p>';
 
-//     contentDiv.innerHTML = html;
-// })
+        contentDiv.innerHTML = html;
+    });
+});
 
-// settingsBtn.addEventListener('click', function() {
-//     changeIcon('settingsIcon');
+settingsBtns.forEach(settingsBtn => {
+    settingsBtn.addEventListener('click', function() {
+        changeIcon('settingsIcon');
 
-//     html = '<p> Settings </p>';
+        html = '<p> Settings </p>';
 
-//     contentDiv.innerHTML = html;
-// })
+        contentDiv.innerHTML = html;
+    });
+});
